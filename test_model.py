d37df784ca243d80b98b7eb9c2d05e0745bbf631
chris bellavia <bella.via.chris@gmail.com>
2019-12-04 08:23:11 -0600

    removed pandas profiling from requirements. should be optional.

--- test_model.py
+++ test_model.py
@@ -10,7 +10,7 @@ from model.labels_reader import LabelsReader
 from model.binary_ensemble import BinaryEnsemble
 import model.cluster_utils as clust
 
-import pandas_profiling
+# import pandas_profiling
 
 # load data
 
@@ -32,7 +32,7 @@ be_dfs = be.get_ensemble()
 
 report_path = "pandas_reports/"
 for frame in be_dfs:
-    pandas_report = frame["X"].profile_report()
-    pandas_report.to_file(output_file=report_path + "unique name")
+    # pandas_report = frame["X"].profile_report()
+    # pandas_report.to_file(output_file=report_path + "unique name")
     clust.scree_plot(frame["X"])
     w = clust.kmeans(frame["X"], 2, title_in="some title")
