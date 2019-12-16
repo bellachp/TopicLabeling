e8d66a2034256332b7fc77dac588884cdf43c91e
chris bellavia <bella.via.chris@gmail.com>
2019-12-16 10:39:08 -0600

    add some cluster utils

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# TopicLabeling
-Code structure/shell for topic labeling project
+## TopicLabeling
+
+Project info
+
+The script makes use of various classes and files in the model folder.
+
+
+#### Dependencies
+Dependencies are managed via virtual environment requirements file. The project makes extensive use of the following open-source libraries:
+
+* https://numpy.org/
+* https://pandas.pydata.org/
+* https://scikit-learn.org/stable/
+* https://xgboost.readthedocs.io/en/latest/
+
+Specific versions can be found in the requirements.txt file.
+
+
+#### virtual environment and setup
+install a virtual environment in your preferred way and use pip to install from the requirements file.
+
+example. from command line:
+```
+pip install virtualenv
+```
+
+navigate to directory desired
+
+```
+virtualenv -p python3 [pathToDir, or . if already there]
+```
+
+activate it
+```
+source [pathToDir]/Scripts/activate ## windows
+source [pathToDir]/bin/activate ## *nix
+```
+
+install from requirements
+```
+pip install -r requirements.txt
+```
+
+
+#### style
+Flake8 and PEP8 are used to check and enforce most python code best practices. Documentation and style guide can be found at the following pages:
+
+http://flake8.pycqa.org/en/latest/
+
+https://www.python.org/dev/peps/pep-0008/
+
+
+###### contact
+```
+url='https://github.com/bellachp/TopicLabeling'
+author='chris bellavia'
+author_email=''
+```
--- model/cluster_utils.py
+++ model/cluster_utils.py
@@ -1,5 +1,10 @@
 # cluster_utils.py
 
+"""
+utility file with clustering & plotting methods
+"""
+
+
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
@@ -11,34 +16,72 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-class ClusterUtils:
+
+def scree_plot(df, title_in="", drop_index=False):
     """
-    utility class with clustering & plotting methods
+    scree plot to view number of potentially viable components
     """
 
-    def __init__(self):
-
-        self.idx = 0
-
-
-    def scree_plot(self, df, title_in="", drop_index=False):
-
-        X = df_in.drop(df_in.columns[0], axis=1)
-        X_std = StandardScaler().fit_transform(X)
-        pca = PCA().fit(X_std)
-        plt.plot(pca.singular_values_)
-        plt.xlabel("num components")
-        plt.ylabel("singular values")
-        title_str = title_in + " " + "scree plot"
-        plt.title(title_str)
-        plt.show()
+    X = df
+    if drop_index:
+        X = df.drop(df.columns[0], axis=1)
+    X_std = StandardScaler().fit_transform(X)
+    pca = PCA().fit(X_std)
+    plt.plot(pca.singular_values_)
+    plt.xlabel("num components")
+    plt.ylabel("singular values")
+    title_str = title_in + " " + "scree plot"
+    plt.title(title_str)
+    plt.show()
 
 
-    def kmeans(self):
+def kmeans(df, num_clusters, title_in="", drop_index=False):
+    """
+    2d kmeans clustering and plotting function
+    plots results along 2 principal components
+    """
 
-        pass
+    X = df
+    if drop_index:
+        X = df.drop(df.columns[0], axis=1)
+    cluster_km = KMeans(n_clusters=num_clusters)
+    cluster_y = cluster_km.fit_predict(X)
+
+    # plotting
+    reduced_data = PCA(n_components=2).fit_transform(X)
+    results = pd.DataFrame(reduced_data, columns=["pc1", "pc2"])
+    cmap = sns.color_palette(n_colors=num_clusters)
+    sns.scatterplot(x="pc1", y="pc2", hue=cluster_y, palette=cmap, data=results)
+    title_str = title_in + " " + str(num_clusters) + " Clusters"
+    plt.title(title_str)
+    plt.show()
+
+    X["cluster"] = cluster_y
+    return X
 
 
-    def kmeans3d(self):
+def kmeans3d(df, num_clusters, title_in="", drop_index=False):
+    """
+    3d kmeans clustering and plotting function
+    plots results along 3 principal components
+    """
 
-        pass
+    X = df
+    if drop_index:
+        X = df.drop(df.columns[0], axis=1)
+    cluster_km = KMeans(n_clusters=num_clusters)
+    cluster_y = cluster_km.fit_predict(X)
+
+    # plotting
+    reduced_data = PCA(n_components=3).fit_transform(X)
+    results = pd.DataFrame(reduced_data, columns=["pc1", "pc2", "pc3"])
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(results["pc1"].values, results["pc2"].values,
+               results["pc3"].values, c=cluster_y)
+    title_str = title_in + " " + str(num_clusters) + " Clusters"
+    plt.title(title_str)
+    plt.show()
+
+    X["cluster"] = cluster_y
+    return X
